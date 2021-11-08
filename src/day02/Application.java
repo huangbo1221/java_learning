d05c6f9efe34a2a106cb343ad8a57533e9c1f9c7
huangbo1221 <1191958036@qq.com>
2021-11-08 23:00:21 +0800

    静态代码块在加载时就会被分配好内存，因此，当new出多个实例时，静态代码块只会被执行一次！

--- src/day02/Application.java
+++ src/day02/Application.java
@@ -12,11 +12,11 @@ public class Application {
     public static void main(String[] args) {
         Child child = new Child();
         // 执行上面这句，输出为
-        // create a father!
+        // person name: this.name   age: 5
         // create a child!
         // 1、说明在子类的构造方法里，隐式的调用了super();方法来执行父类的无惨构造方法
-        // 2、当父类的构造方法只有有参法人构造方法时，若子类不显示调用super(参数)来执行父类的
-        // 构造方法，编译会报错。所以当给父类有参构造方法后，一般是要显示的写出无参构造方法
+        // 2、当父类的构造方法只有有参构造方法时，若子类不显示调用super(参数)来执行父类的
+        // 构造方法，编译会报错。当给父类有参构造方法后，一般是要显示的写出无参构造方法。
         // 3、super()方法必须出现在子类构造方法的第一句，否则编译报错！！！
 
         child.test("huangbo1221");
